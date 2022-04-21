029c5ec95509f83a7e80809f10690a6aea1eb056
Renato Caçula <renatocacula2015@gmail.com>
2022-04-21 05:13:22 -0300

    Schema add

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
 
+const PrismaClient = require('@prisma/client')
+const prisma = new PrismaClient()
+
+
 
 app.listen(3000, () => console.info('Server running on port 3000') )
