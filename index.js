9a985362c8ed672667bbfe3fa35c0243fcd1468f
Renato Caçula <renatocacula2015@gmail.com>
2022-04-22 17:20:07 -0400

    README add

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-const PrismaClient = require('@prisma/client')
-const prisma = new PrismaClient()
-
-
-
-app.listen(3000, () => console.info('Server running on port 3000') )
